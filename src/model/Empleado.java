5af49f4ef2ce928cca79da8db373ee00fb42d0a2
Mauricio Borgogno <106617850+Muari1999@users.noreply.github.com>
2026-05-20 20:51:45 -0300

    asd

--- src/model/Empleado.java
+++ src/model/Empleado.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Empleado {
+	private String nombre;
+	private Roles rol;
+	private int calificacion;
+	
+	public Empleado (String nombre, Roles rol, int calificacion) {
+	    this.nombre = nombre;
+	    if(nombre == null || nombre.isBlank()) {
+	        throw new IllegalArgumentException("El nombre no puede estar vacío");
+	    }
+	    this.rol = rol;
+	    this.calificacion = calificacion;
+	    if(calificacion < 1 || calificacion > 5) {
+	        throw new IllegalArgumentException("La calificación debe estar entre 1 y 5");
+	    }
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
 
+	public Roles getRol() {
+		return rol;
+	}
+
+	public int getCalificacion() {
+		return calificacion;
+	}
 }
